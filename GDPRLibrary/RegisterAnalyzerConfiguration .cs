1d5763623f2d1a51a7be6c5cf6abe729dd46e71f
Søren Schytz Birk <ssb@rybners.dk>
2024-09-16 19:59:21 +0200

    Add project files.

---
+++ GDPRLibrary/RegisterAnalyzerConfiguration .cs
@@ -0,0 +1,14 @@
+﻿using UiPath.Studio.Activities.Api;
+using UiPath.Studio.Activities.Api.Analyzer;
+using UiPath.Studio.RulesLibrary.Rules.Naming;
+
+namespace GDPRLibrary
+{
+    public class RegisterAnalyzerConfiguration : IRegisterAnalyzerConfiguration
+    {
+        public void Initialize(IAnalyzerConfigurationService workflowAnalyzerConfigService)
+        {
+
+        }
+    }
+}
