b3ad5010e63c85cdacf9e56b4b74f35eb6b4f57e
Søren Schytz Birk <ssb@rybners.dk>
2024-09-18 10:00:19 +0200

    Changes to CPR-number rule.

--- GDPRLibrary/gdpr_cpr_check.cs
+++ GDPRLibrary/gdpr_cpr_check.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using UiPath.Studio.Activities.Api;
+using UiPath.Studio.Activities.Api.Analyzer;
 using UiPath.Studio.Activities.Api.Analyzer.Rules;
 using UiPath.Studio.Analyzer.Models;
 
@@ -17,10 +20,71 @@ namespace GDPRLibrary
         {
             var rule = new Rule<IActivityModel>("GDPR - Check for CPR number in code.", RuleId, Inspect)
             {
-                RecommendationMessage = "GDPR - Check for CPR number in code.",
+                RecommendationMessage = "GDPR - Checks for danish personal identification number (CPR-number) inside the code.",
                 ErrorLevel = System.Diagnostics.TraceLevel.Error,
             };
-            return rule;
+            rule.Parameters.Add(RegexKey, new Parameter()
+        }
+        private static InspectionResult Inspect(IActivityModel activityModel, Rule ruleInstance)
+        {
+            var setRegexValue = ruleInstance.Parameters[RegexKey]?.Value;
+
+            var regex = new Regex(setRegexValue);
+            var messageList = new List<InspectionMessage>();
+
+            if (string.IsNullOrEmpty(setRegexValue))
+            {
+                return new InspectionResult()
+                {
+                    HasErrors = false
+                };
+            }
+
+            if (!String.IsNullOrEmpty(activityModel.DisplayName) && Regex.IsMatch(activityModel.DisplayName, setRegexValue))
+            {
+                messageList.Add(new InspectionMessage()
+                {
+                    Message = $"The Activity '{activityModel.DisplayName}' contains CPR-number in DisplayName!",
+                });
+            };
+
+            if (!String.IsNullOrEmpty(activityModel.AnnotationText) && Regex.IsMatch(activityModel.AnnotationText, setRegexValue))
+            {
+                messageList.Add(new InspectionMessage()
+                {
+                    Message = $"Aktiviteten '{activityModel.DisplayName}' indeholder cpr-nummer i AnnotationText!",
+                });
+            };
+
+            if (activityModel.Arguments.Count == 0 & activityModel.Variables.Count == 0 & messageList.Count == 0)
+            {
+                return new InspectionResult()
+                {
+                    HasErrors = false
+                };
+            }
+
+            foreach (var argument in activityModel.Arguments)
+            {
+                if (!String.IsNullOrEmpty(argument.DefinedExpression) && Regex.IsMatch(argument.DefinedExpression, setRegexValue))
+                {
+                    messageList.Add(new InspectionMessage()
+                    {
+                        Message = $"Aktiviteten '{activityModel.DisplayName}' indeholder cpr-nummer!",
+                    });
+                };
+            }
+
+            foreach (var variable in activityModel.Variables)
+            {
+                if (!String.IsNullOrEmpty(variable.DefaultValue) && Regex.IsMatch(variable.DefaultValue, setRegexValue))
+                {
+                    messageList.Add(new InspectionMessage()
+                    {
+                        Message = $"Variablen '{variable.DisplayName}' indeholder cpr-nummer!",
+                    });
+                };
+            }
         }
     }
 }
